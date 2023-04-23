88d61f97b5f56a7cb6c4ba1d36aecf36792be1c5
Jed <vjeudy@protonmail.com>
2023-04-23 20:36:11 +0200

    adds config files

---
+++ rspack.config.mjs
@@ -0,0 +1,58 @@
+import path from 'path'
+
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  target: 'web',
+  entry: {
+    main: './src/index.js',
+    style: './src/main.css',
+  },
+  optimization: {
+    minimize: process.env.NODE_ENV === 'production',
+  },
+  output: {
+    filename: '[name][ext]',
+    path: path.resolve('dist'),
+  },
+  resolve: {
+    extensions: ['*', '.js', 'scss'],
+    alias: {
+      '@': './src',
+      'wasm-lib': './wasm-lib/pkg/wasm_lib.js',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: {
+                  tailwindcss: {
+                    config: './taiwlind.config.js',
+                  },
+                  autoprefixer: {},
+                },
+              },
+            },
+          },
+        ],
+        type: 'css',
+      },
+    ],
+  },
+  devServer: {
+    watchFiles: ['index.html', 'src/**'],
+  },
+  builtins: {
+    html: [
+      {
+        template: 'index.html',
+      },
+    ],
+  },
+  stats: 'verbose',
+}
